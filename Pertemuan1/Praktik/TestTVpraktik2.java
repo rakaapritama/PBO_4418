1040c5e92f04cc043c925cccb84adc80ce862603
rakaapritama <raka.apritama3@gmail.com>
2021-03-10 12:03:13 +0700

    Peanambahan class TV

--- Pertemuan1/Praktik/TestTVpraktik2.java
+++ Pertemuan1/Praktik/TestTVpraktik2.java
@@ -9,25 +9,16 @@
  * @author Raka Apritama
  */
 public class TestTVpraktik2 {
-
-  
-//    @param args the command line arguments
-    int channel; 
-//    make channel variable
-    int volumeLevel;
-//    make volumeLevel variable
-    boolean on;
-//     make on variable
     
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         // TODO code application logic here
-        TestTVpraktik2 tv1 = new TestTVpraktik2();
-        tv1.turnOn(true);
+        TV tv1 = new TV();
+        tv1.turnOn();
         tv1.setChannel(30);
         tv1.setVolume(3);
         
-        TestTVpraktik2 tv2 = new TestTVpraktik2();
-        tv2.turnOn(true);
+        TV tv2 = new TV();
+        tv2.turnOn();
         tv2.channelUp();
         tv2.volumeUp();
         tv2.channelUp();
@@ -38,41 +29,49 @@ public class TestTVpraktik2 {
         System.out.println("tv1's channel is " + tv1.channel + " and volume is " + tv1.volumeLevel);
         System.out.println("tv2's channel is " + tv2.channel + " and volume is " + tv2.volumeLevel);
     }
+}
 
-    //  object construction
-    TestTVpraktik2() { 
-    } 
-    
-//    Tv condition On
-    void turnOn(boolean on) {
+//Make TV Class    
+class TV {
+
+    int channel; 
+    // make channel variable
+    int volumeLevel;
+    //make volumeLevel variable
+    boolean on = false;
+    //make on variable
+        
+    public TV(){}
+    //Tv condition On
+    public void turnOn() {
         on = true;
     }
-//    Tv condition Off
-    void turnOff(boolean on) {
+    //Tv condition Off
+    public void turnOff() {
         on = false;
     }
-//    indicator to set Channel
-    void setChannel(int newChannel){
+    //indicator to set Channel
+    public void setChannel(int newChannel){
         channel = newChannel;
     }
-//    indicator to set Volume
-    void setVolume(int newVolumeLevel){
+    //indicator to set Volume
+    public void setVolume(int newVolumeLevel){
         volumeLevel = newVolumeLevel;
     }
-//      channel was up
-    void channelUp() {
+    //channel was up
+    public void channelUp() {
         ++channel ;
     }
-//    channel was down
-    void channelDown() {
+    //channel was down
+    public void channelDown() {
         --channel;
     }
-//    volume was up
-    void volumeUp() {
+    //volume was up
+    public void volumeUp() {
         ++volumeLevel;
     }
-//    volume was down
-    void volumeDown() {
+    //volume was down
+    public void volumeDown() {
         --volumeLevel ;
     }
 }
